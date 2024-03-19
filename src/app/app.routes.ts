5a351c2a69fb0408d399a2f994ffaa16ebf1ea08
Tab0o33 <w.moinet@orange.fr>
2024-03-19 19:59:37 +0100

    feat: creation de la homepage et d routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        loadComponent: () => import('./views/homepage/homepage.component').then((x) => x.HomepageComponent),
+        title: 'Accueil'
+    }
+];
---
+++ src/app/views/homepage/homepage.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+
+@Component({
+    selector: 'app-homepage',
+    standalone: true,
+    imports: [MatCardModule],
+    templateUrl: './homepage.component.html',
+    styles: ``
+})
+export class HomepageComponent {
+
+}
